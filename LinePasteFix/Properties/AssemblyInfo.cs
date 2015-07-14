88b584c6b2eaa0c9d14f242e872892c21865a5e4
Chris McGrath <admin@cpmcgrath.com>
2015-07-14 11:50:50 +1000

    Add support for VS2015. Closes #2

--- LinePasteFix/Properties/AssemblyInfo.cs
+++ LinePasteFix/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture      ("")]
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("6.0.0.0")]
-[assembly: AssemblyFileVersion("6.0.0.0")]
+[assembly: AssemblyVersion("7.0.0.0")]
+[assembly: AssemblyFileVersion("7.0.0.0")]
